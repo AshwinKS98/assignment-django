8949775b3bc08ba63e77832bf66d18e8ccc20548
AshwinKS98 <107168856+AshwinKS98@users.noreply.github.com>
2024-10-11 15:15:14 +0530

    Update manage.py

--- signals_project/manage.py
+++ signals_project/manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     # Set the settings module to development settings
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signals_project.settings.development')  # Update this line
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signals_project.settings.development') 
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
